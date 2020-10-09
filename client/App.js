a018454f0e90b62209f489a3a76d548ad5cb580b
Sara Björk <saraellenbjork@gmail.com>
2020-10-09 15:33:50 +0200

    Alla sidor tillagda

--- client/App.js
+++ client/App.js
@@ -1,77 +1,15 @@
-import { StatusBar } from "expo-status-bar";
+/* #8 för hur vi kan spara data för vilken knapp användaren trycker på i quizet*/
+/* #10 för hur vi kan skapa papperskorgen när vi vill slänga en dryck i mina drycker */
+
 import React, { useState } from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-} from "react-native";
+import Home from "./screens/home";
 
 export default function App() {
   const [name, setName] = useState("sara");
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <Text>Mina drycker</Text>
-        <StatusBar style="auto" />
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.logga}>Dryckesakuten</Text>
-        <Text style={styles.bodyText}>
-          Generera den ultimata drycken att köpa på ditt Systembolag.
-        </Text>
-        <TouchableOpacity style={styles.startButton}>
-          <Text style={styles.buttonText}>Start</Text>
-        </TouchableOpacity>
-        <Text style={styles.fotter}>Inget valt bolag</Text>
-      </View>
-    </SafeAreaView>
+    /* Kanske inte använda SafeAreaView och padda istället så att
+        färgen når hela vägen upp. */
+    <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  header: {
-    flex: 1,
-    backgroundColor: "pink",
-  },
-  body: {
-    flex: 8,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
-  },
-  logga: {
-    fontSize: 40,
-    fontWeight: "bold",
-    backgroundColor: "yellow",
-  },
-  bodyText: {
-    textAlign: "center",
-    fontSize: 16,
-    backgroundColor: "blue",
-    paddingLeft: 50,
-    paddingRight: 50,
-  },
-  startButton: {
-    backgroundColor: "#36575C",
-    margin: 50,
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
-    textAlign: "center",
-    padding: 15,
-  },
-  fotter: {
-    fontWeight: "bold",
-    backgroundColor: "green",
-  },
-});
