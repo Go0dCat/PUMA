c28a981eacba09ec9586ba8649488e8d81bfa012
Sara Jonsson <zara.ztrom@gmail.com>
2020-10-09 15:36:23 +0200

    added addition to db by subcategory and 10 items each

--- server/routes/systembolaget.js
+++ server/routes/systembolaget.js
@@ -36,29 +36,62 @@ router.get('/systembolaget', function(req, res, next){
 });
 
 //search
+
+//TODO add '/systembolaget/search/:searchstring'
 router.get('/systembolaget/search', function(req, res, next){
   console.log('req to systembolaget');
 
   var searchArray = [
       'Rött vin',
       'Vitt vin',
-      'Öl'
+      'Öl',
+      'Cider',
+      'Blanddrycker',
+      'Mousserande vin',
+      'Rosévin'
     ];
 
-  axios.get('https://api-extern.systembolaget.se/product/v1/product', {
-    headers: {'Ocp-Apim-Subscription-Key': '06fdbc8b8df845b3840710ba53db9009'}}
-  ).then(response => {
-    console.log('res from systembolaget');
-    console.log(response.data);
-    //console.log(response.data.explanation);
+    var isDone = false;
+    //
+  searchArray.forEach((item, i) => {
+    axios.get('https://api-extern.systembolaget.se/product/v1/product/search?SubCategory=' + item, {
+      headers: {'Ocp-Apim-Subscription-Key': '06fdbc8b8df845b3840710ba53db9009'}})
+        .then(response => {
+          console.log('res from systembolaget');
+          //console.log(response.data.Hits[0]);
+          //res.send(response.data.Hits[0]);
 
-    //res.send(response.data;
-  })
-  .catch(error => {
-    console.log(error);
-  })
+          //TODO fix check for duplicates
+          //TODO fix seperate function for add to db
 
-  res.send({systembolaget: 'yes'});
+          for(j = 0; j <10; j++) {
+            Product.create({
+              ProductId: response.data.Hits[j].ProductId,
+              ProductNumber: response.data.Hits[j].ProductNumber,
+              ProductNameBold: response.data.Hits[j].ProductNameBold,
+              ProductNameThin: response.data.Hits[j].ProductNameThin,
+              Category: response.data.Hits[j].Category,
+              BottleTextShort: response.data.Hits[j].BottleTextShort,
+              Seal: response.data.Hits[j].Seal,
+              AlcoholPercentage: response.data.Hits[j].AlcoholPercentage,
+              Volume: response.data.Hits[j].Volume,
+              Price: response.data.Hits[j].Price,
+              SubCategory: response.data.Hits[j].SubCategory,
+              Type: response.data.Hits.Type
+            }).then(function(){
+            console.log(item + ' product added to db');
+            //res.send(product);
+            }).catch(next);
+          }
+
+       //res.send(response.data);
+      }).catch(error => {
+        console.log(error);
+
+      });
+    });
+
+    //res.send({systembolaget: 'done'});
 });
 
 
