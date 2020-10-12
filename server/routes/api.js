3c43987e0618c2f5668c81fe181b6e4ad4e4e9da
Sara Jonsson <zara.ztrom@gmail.com>
2020-10-12 15:15:39 +0200

    added rest api routing for subcategory and amount

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,7 +9,7 @@ const Product = require('../models/product');
 //gets all products
 router.get('/products', function(req, res, next){
   console.log('req all products');
-  Product.find().then(funciton(products){
+  Product.find().then(function(products){
     res.send(products);
   }).catch(next);
 });
@@ -20,6 +20,30 @@ router.get('/category/:category', function(req, res, next){
   Product.find({SubCategory: req.params.category}).then(function(products){
     console.log('res with category');
     res.send(products);
+  }).catch(next);
+});
+
+router.get('/category/:category/:number', function(req, res, next){
+  console.log('req products by category');
+  Product.find({SubCategory: req.params.category}).then(function(products){
+    var result = [];
+    var numbers = [];
+
+    for (let i = 0; i < Math.min(req.params.number, products.length); i++) {
+      let foundX = false;
+      do {
+        let x = Math.floor(Math.random() * products.length);
+        if(!numbers.includes(x)) {
+          result.push(products[x]);
+          numbers.push(x);
+          foundX = true;
+          //console.log('found and added' + x);
+        }
+        //console.log('found ' + x);
+      } while(!foundX);
+    }
+    console.log('res with category by x numbers');
+    res.send(result);
   }).catch(next);
 });
 
