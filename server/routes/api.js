dd21841b7ed452d625a93c1e10caf039b346be7f
Sara Jonsson <zara.ztrom@gmail.com>
2020-10-12 13:55:49 +0200

    added basic rest api routing

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,12 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 //require models here...
+const Product = require('../models/product');
 
 
 //routing
-router.get('/test', function(req, res, next){
-  console.log('req to test/get');
-  res.send({test: 'yes'});
+//gets all products
+router.get('/products', function(req, res, next){
+  console.log('req all products');
+  Product.find().then(funciton(products){
+    res.send(products);
+  }).catch(next);
+});
+
+//gets product by subcategory
+router.get('/category/:category', function(req, res, next){
+  console.log('req products by category');
+  Product.find({SubCategory: req.params.category}).then(function(products){
+    console.log('res with category');
+    res.send(products);
+  }).catch(next);
 });
 
 module.exports = router;
