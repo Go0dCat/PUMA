18501300d71ef8150c9d642bebb0a6463a1ae0f2
Sara Björk <saraellenbjork@gmail.com>
2020-10-14 15:21:57 +0200

    Screens och start pa navigation

---
+++ client/screens/quiz-1.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, FlatList, Image } from "react-native";
+import { globalStyles, images } from "../styles/global";
+
+export default function Quiz1({ navigation }) {
+  const [situations, setSituation] = useState([
+    { title: 'me-time', key: '1' },
+    { title: 'dinner-friends', key: '2' },
+    { title: 'boardgame', key: '3' },
+    { title: 'party', key: '4' },
+    { title: 'dinner-romantic', key: '5' },
+    { title: 'BBQ', key: '6' },
+  ])
+
+  let quizAnswers = [];
+  //const situation = navigation.getParam('title');
+
+  return (
+    <View style={globalStyles.quizContainer}>
+      <Text>Fråga 1 - Vad bjuder kvällen på?</Text>
+
+      <FlatList
+        style={globalStyles.quizAnswers}
+        //horizontal={false}
+        numColumns={2}
+        data={situations}
+        renderItem={({ item }) => (
+          // Fixa så att objektet både sparas och arrayen skickas med i onPress
+          <TouchableOpacity style={globalStyles.quizAnswer} onPress={() => quizAnswers.push(item), () => navigation.navigate('Quiz2', quizAnswers)}>
+            <Image source={images.situations[item.title]} />
+          </TouchableOpacity>
+        )}
+      />
+
+
+    </View>
+  );
+}
--- client/screens/quiz-4.js
+++ client/screens/quiz-4.js
@@ -2,6 +2,6 @@ import React, { useState } from "react";
 import { Text } from "react-native";
 import { globalStyles } from "../styles/global";
 
-export default function MyBev() {
-  return <Text>Myna drycker</Text>;
+export default function Quiz4() {
+  return <Text>Vad är viktigast för dig?</Text>;
 }
