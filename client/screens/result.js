21fc6cb9bb68baa3ddb89e829fa4c4cd5403dbd9
Jennifer <jenniferlillemor@gmail.com>
2020-10-22 14:10:32 +0200

    Update result.js

--- client/screens/result.js
+++ client/screens/result.js
@@ -1,47 +1,39 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Card, ListItem, Button, Icon } from 'react-native-elements';
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { globalStyles } from "../styles/global";
 
 export default function Result({ navigation }) {
 
-/*export default class Result extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      dataSource: null,
-    }
-  }*/
-
-  /*render() {
-    if (this.state.isLoading) {
-      return (
-        <View>ActivityIndicator</View>
-      )
-    } else {
-      return (
-        <View>Yay</View>
-      )
-    }
-  }*/
-
   // TODO: Fixa så att 'responseJson[0].ProductId' syns i vyn
-  function fetchProduct() {
-    console.log('fetch');
-    fetch('http://192.168.1.223:8081/api/client/category/Öl')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log('Response: ' + responseJson[0].ProductId);
-        return responseJson[0].ProductId;
-      })
-      .catch((error) => {
-        console.log(error);
-        return 'hej';
-      });
-      console.log('utanför');
-      return 'test';
-  }
+  
+
+ /* function testFunction() {
+
+    const [value, setValue] = useState(null);
+
+    useEffect(() => {
+      console.log('fetch');
+        fetch('http://172.23.130.126:8081/api/client/category/Öl')
+          .then((response) => response.json())
+          .then((responseJson) => {
+            console.log('Response: ' + responseJson[0].ProductId);
+            setValue({value: responseJson[0].ProductId});
+          })
+          .catch((error) => {
+            console.log(error);
+            //return 'hej';
+          });
+        console.log('utanför');
+        //return 'test';
+    });
+
+    if (value === null) {
+      return 'Loading...';
+    }
+    return value;
+  }*/
+  
 
   const pressHandler = () => {
     // Kolla om det finns en "pop all" för att navigera till Home.
@@ -49,13 +41,71 @@ export default function Result({ navigation }) {
   };
 
   return (
-    <View>
-      <Text>Din genererade dryck</Text>
-      <Text>Produkt: {fetchProduct()}</Text>
-      <Text></Text>
-      <TouchableOpacity onPress={pressHandler}>
-        <Text>Tillbaka till start.</Text>
-      </TouchableOpacity>
+    <View style={styles.viewContainer}>
+
+      <View style={styles.firstContainer}>
+        <View style={styles.productContainer}>
+          <Text style={styles.productBold}>Ahlafors</Text>
+          <Text style={styles.productThin}>Peach Passion</Text>
+        </View>
+        <View style={styles.priceContainer}>
+          <Text style={styles.price}>24:18</Text>
+          <Text style={styles.volume}>330 ml</Text>
+        </View>
+      </View>
+
+      <View style={styles.attributeContainer}>
+        <Text style={styles.bold}>Kategori</Text>
+        <Text>Cider</Text>
+      </View>
+
+      <View style={styles.attributeContainer}>
+        <Text style={styles.bold}>Alkoholhalt</Text>
+        <Text>4,5 %</Text>
+      </View>
+         
     </View>
   );
 }
+
+const styles = StyleSheet.create ({
+
+  viewContainer: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: 'white',
+  },
+  firstContainer: {
+    flexDirection: 'row',
+  },
+  productContainer: {
+    width: '70%',
+  },
+  priceContainer: {
+    width: '30%',
+  },
+  attributeContainer: {
+    marginTop: 15,
+  },
+
+  productBold: {
+    fontSize: 19,
+    fontWeight: 'bold',
+  },
+  productThin: {
+    fontSize: 17,
+  },
+  price: {
+    textAlign: 'right',
+    fontSize: 19,
+    fontWeight: 'bold',
+  },
+  volume: {
+    textAlign: 'right',
+    fontSize: 13,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  
+})
